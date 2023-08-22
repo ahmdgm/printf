33e0d763a05e8e0ae91fdf0049bd0eaad31ec34c
Anas hamed ahmed hamed <111208532+Anas2108@users.noreply.github.com>
2023-08-22 20:06:10 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -72,3 +72,71 @@ int _printf(const char *format, ...)
 	va_end(ptr); 
 	return counter; 
 }
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/*
+* print_int - function to print int numbers
+* @value: value passed to print it
+* Return: int 
+*/
+int print_int(int val)
+{
+	char buffer[32]; 
+	int printed = 0; 
+    int i = 0;
+
+	if (val < 0)
+	{
+		_putchar('-');
+		printed++;
+		val = -val;
+	}
+
+	do
+	{
+		buffer[i++] = '0' + (val % 10);
+		val /= 10;
+		printed++;
+	} while (val > 0);
+	for (int j = i - 1; j >= 0; j--)
+	{
+		_putchar(buffer[j]);
+	}
+
+	return printed;
+}
+
+
+
+/**
+* print_buff - function to print buffer
+* @buffer: array of characters passed
+* @buff_ind: is a pointer to an integer
+*/
+void print_buff(char buffer[], int *buff_index)
+{
+	if (*buff_index > 0)
+	{
+		for (int i = 0; i < *buff_index; i++)
+		{
+			_putchar(buffer[i]);
+		}
+	}
+
+	*buff_index = 0;
+}
+
+
