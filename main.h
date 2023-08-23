8d3fcb7137d0db230a187a3e8e82116c06658da5
Anas hamed ahmed hamed <111208532+Anas2108@users.noreply.github.com>
2023-08-23 03:28:49 +0300

    Update main.h

--- main.h
+++ main.h
@@ -14,21 +14,3 @@ int print_int(int value);
 void print_buff(char buffer[], int *buff_ind);
 
 #endif
-=======
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-
-#define BUFF_SIZE 1024
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_int(int value);
-void print_buff(char buffer[], int *buff_ind);
-
-#endif
